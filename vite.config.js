01d0ac56eee8639ce271da7a9d3c9af5b9686cdd
JacobJanashvili <iakobijanashvili7@gmail.com>
2023-07-23 16:55:26 +0400

    idk

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: "/jacobjanashvili.github.io/bgs/",
+  base: "/bgs/",
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
